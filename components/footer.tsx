461351a42cfae1c4d982ee6353e57212ccbb9617
95tuanle <tlna95@icloud.com>
2023-12-30 04:35:39 -0500

    improve footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,7 @@
 const Footer = ({gitHubUser, phoneNumber}: any) => <footer className="p-3 text-center">
+  <a href={'/'} className="hover:underline"
+     aria-label={"Main page"}>&copy; {new Date().getFullYear()} {gitHubUser["name"]}</a>
   <p className="mb-3">
-    <a href={'/'} className="hover:underline"
-       aria-label={"Main page"}>&copy; {new Date().getFullYear()} {gitHubUser["name"]}</a>
-    <span className="mx-3">|</span>
     <a href={`mailto:${gitHubUser["email"]}`} className="hover:underline">{gitHubUser["email"]}</a>
     <span className="mx-3">|</span>
     <a href={`tel:${phoneNumber}`} className="hover:underline">{phoneNumber}</a>
