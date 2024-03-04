4c13bc5377ce4561073e6a88f183fe0ea2f14c65
95tuanle <tlna95@icloud.com>
2024-03-04 15:30:20 -0500

    let's try again

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,17 @@
 const Footer = ({ gitHubUser, phoneNumber }: any) => (
   <footer className="p-3 text-center">
-    <a href={'/'} className="hover:underline" aria-label={'Main page'}>
+    <a href={'/'} className="hover:underline py-3" aria-label={'Main page'}>
       &copy; {new Date().getFullYear()} {gitHubUser['name']}
     </a>
     <p className="mb-3">
-      <a href={`mailto:${gitHubUser['email']}`} className="hover:underline">
+      <a
+        href={`mailto:${gitHubUser['email']}`}
+        className="hover:underline py-3"
+      >
         {gitHubUser['email']}
       </a>
       <span className="mx-3">|</span>
-      <a href={phoneNumber} className="hover:underline">
+      <a href={phoneNumber} className="hover:underline py-3">
         {phoneNumber.replace('tel:', '')}
       </a>
     </p>
