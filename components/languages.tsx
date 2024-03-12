8c14b317345013d4cb78aa65f7d8b72079aaee92
95tuanle <tlna95@icloud.com>
2024-03-11 23:06:55 -0400

    condense

--- components/languages.tsx
+++ components/languages.tsx
@@ -5,16 +5,14 @@ const Languages = async ({ owner, repo }: any) => {
     const languages = await getGitHubUserRepoLanguages(owner, repo);
     return (
       <div className="flex flex-wrap">
-        {Object.keys(languages).map((language) => {
-          return (
-            <div
-              key={language}
-              className="bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 rounded-full px-2 py-1 text-xs font-semibold m-1"
-            >
-              {language}
-            </div>
-          );
-        })}
+        {Object.keys(languages).map((language) => (
+          <div
+            key={language}
+            className="bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 rounded-full px-2 py-1 text-xs font-semibold m-1"
+          >
+            {language}
+          </div>
+        ))}
       </div>
     );
   } catch (error) {
