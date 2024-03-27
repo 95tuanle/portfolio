8afdfc15ba176715bc0bff1cafacc68433cc94d4
95tuanle <tlna95@icloud.com>
2024-03-27 04:29:47 -0400

    make components to render on server side instead of client side

--- components/projects.client.tsx
+++ components/projects.client.tsx
@@ -1,31 +1,32 @@
 'use client';
-import { useState } from 'react';
-import Project from '@/components/project';
+import { ReactNode, useState } from 'react';
 
-const ProjectsClient = ({ gitHubUserRepos }: any) => {
+const ProjectsClient = ({ children }: { children: ReactNode }) => {
   const [sliceThreshold, setSliceThreshold] = useState(0);
   return (
     <>
-      {gitHubUserRepos.slice(0, sliceThreshold).map((gitHubUserRepo: any) => (
-        <Project gitHubUserRepo={gitHubUserRepo} key={gitHubUserRepo['id']} />
-      ))}
-      <div className="text-center p-3">
-        {sliceThreshold < gitHubUserRepos.length ? (
-          <button
-            className="bg-gray-700 dark:bg-gray-300 text-gray-300 dark:text-gray-700 rounded-full px-2 py-1 text-xs font-semibold m-1"
-            onClick={() => setSliceThreshold(sliceThreshold + 10)}
-          >
-            Load More
-          </button>
-        ) : (
-          <button
-            className="bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-gray-300 rounded-full px-2 py-1 text-xs font-semibold m-1"
-            onClick={() => setSliceThreshold(0)}
-          >
-            Load Less
-          </button>
-        )}
-      </div>
+      {Array.isArray(children) ? (
+        <>
+          {children.slice(0, sliceThreshold).map((child: any) => child)}
+          <div className="text-center p-3">
+            {sliceThreshold < children.length ? (
+              <button
+                className="bg-gray-700 dark:bg-gray-300 text-gray-300 dark:text-gray-700 rounded-full px-2 py-1 text-xs font-semibold m-1"
+                onClick={() => setSliceThreshold(sliceThreshold + 10)}
+              >
+                Load More
+              </button>
+            ) : (
+              <button
+                className="bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-gray-300 rounded-full px-2 py-1 text-xs font-semibold m-1"
+                onClick={() => setSliceThreshold(0)}
+              >
+                Load Less
+              </button>
+            )}
+          </div>
+        </>
+      ) : null}
     </>
   );
 };
