71bbbcf61495d927c7f5ee60927d8861f5d00b20
95tuanle <tlna95@icloud.com>
2024-04-02 04:32:52 -0400

    use loading instead of suspense

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,7 @@
-export const Loading = () => (
+const Loading = () => (
   <div className="flex items-center justify-center h-screen animate-pulse">
     Loading...
   </div>
 );
+
+export default Loading;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,113 @@
-import { Suspense } from 'react';
-import { Loading } from '@/components/loading';
-import { Body } from '@/components/body';
+import {
+  getGitHubUser,
+  getGitHubUserRepoReadmeFileContent,
+  getGitHubUserRepos,
+} from '@/ultilities/github';
+import { getLeetCodeUserProfileSubmissionNumbers } from '@/ultilities/leetcode';
+import {
+  GitHubReadmeMarkdownJSON,
+  parseGitHubReadmeMarkdown,
+} from '@/ultilities/github-readme-markdown-parser';
+import User from '@/components/user';
+import Experience from '@/components/experience/experience';
+import Education from '@/components/education/education';
+import Projects from '@/components/projects/projects';
+import LeetCode from '@/components/leetcode/leetcode';
+import Contact from '@/components/contact/contact';
+import Footer from '@/components/footer';
 
-const Page = () => (
-  <Suspense fallback={<Loading />}>
-    <Body />
-  </Suspense>
-);
+const Page = async () => {
+  try {
+    const gitHubUser = await getGitHubUser();
+    const [
+      gitHubUserRepos,
+      gitHubUserRepoFileContent,
+      leetCodeUserProfileSubmissionNumbers,
+    ] = await Promise.all([
+      getGitHubUserRepos(),
+      getGitHubUserRepoReadmeFileContent(
+        gitHubUser['login'],
+        gitHubUser['login'],
+      ),
+      getLeetCodeUserProfileSubmissionNumbers(gitHubUser['login']),
+    ]);
+    const iconSize = 28;
+    const numberOfProjects = 20;
+    let phoneNumber: string | any;
+    let leetCodeUrl: string | any;
+    let linkedInUrl: string | any;
+    let stackOverflowUrl: string | any;
+    let wellfoundUrl: string | any;
+    let redditUrl: string | any;
+    let soundCloudUrl: string | any;
+    let gitHubReadmeMarkdownJSON: GitHubReadmeMarkdownJSON | any;
+    if (typeof gitHubUserRepoFileContent == 'string') {
+      gitHubReadmeMarkdownJSON = parseGitHubReadmeMarkdown(
+        gitHubUserRepoFileContent,
+      );
+      for (const gitHubReadmeMarkdownJSONContact of gitHubReadmeMarkdownJSON[
+        'contactInformation'
+      ]) {
+        switch (gitHubReadmeMarkdownJSONContact['type']) {
+          case 'Phone':
+            phoneNumber = gitHubReadmeMarkdownJSONContact['url'];
+            break;
+          case 'LeetCode':
+            leetCodeUrl = gitHubReadmeMarkdownJSONContact['url'];
+            break;
+          case 'LinkedIn':
+            linkedInUrl = gitHubReadmeMarkdownJSONContact['url'];
+            break;
+          case 'Stack Overflow':
+            stackOverflowUrl = gitHubReadmeMarkdownJSONContact['url'];
+            break;
+          case 'Wellfound':
+            wellfoundUrl = gitHubReadmeMarkdownJSONContact['url'];
+            break;
+          case 'Reddit':
+            redditUrl = gitHubReadmeMarkdownJSONContact['url'];
+            break;
+          case 'SoundCloud':
+            soundCloudUrl = gitHubReadmeMarkdownJSONContact['url'];
+            break;
+        }
+      }
+    }
+    return (
+      <>
+        <User
+          gitHubUser={gitHubUser}
+          phoneNumber={phoneNumber}
+          leetCodeUrl={leetCodeUrl}
+          linkedInUrl={linkedInUrl}
+          stackOverflowUrl={stackOverflowUrl}
+          wellfoundUrl={wellfoundUrl}
+          redditUrl={redditUrl}
+          soundCloudUrl={soundCloudUrl}
+          iconSize={iconSize}
+        />
+        <Experience experience={gitHubReadmeMarkdownJSON['experience']} />
+        <Education education={gitHubReadmeMarkdownJSON['education']} />
+        <Projects
+          gitHubUserRepos={gitHubUserRepos}
+          numberOfProjects={numberOfProjects}
+        />
+        <LeetCode
+          leetCodeUserProfileSubmissionNumbers={
+            leetCodeUserProfileSubmissionNumbers
+          }
+        />
+        <Contact
+          gitHubUserEmail={gitHubUser['email']}
+          gitHubUserName={gitHubUser['name']}
+        />
+        <Footer gitHubUser={gitHubUser} phoneNumber={phoneNumber} />
+      </>
+    );
+  } catch (error) {
+    console.error(error);
+    return <>Something went wrong.</>;
+  }
+};
 
 export default Page;
