7d0dba51f6982237d3ce2f1516a0005529a9b5ab
95tuanle <tlna95@icloud.com>
2024-04-17 05:00:32 -0400

    add divs

--- components/projects/project.sub.tsx
+++ components/projects/project.sub.tsx
@@ -2,30 +2,34 @@ import TopicsOrLanguages from '@/components/projects/topics-or-languages';
 
 const ProjectSub = ({ gitHubUserRepo }: any) => (
   <>
-    <a
-      className="text-lg font-semibold mb hover:underline"
-      href={gitHubUserRepo['html_url']}
-      target={'_blank'}
-      aria-label={gitHubUserRepo['name']}
-      title={gitHubUserRepo['name']}
-    >
-      {gitHubUserRepo['name']}
-    </a>
+    <div>
+      <a
+        className="text-lg font-semibold mb hover:underline"
+        href={gitHubUserRepo['html_url']}
+        target={'_blank'}
+        aria-label={gitHubUserRepo['name']}
+        title={gitHubUserRepo['name']}
+      >
+        {gitHubUserRepo['name']}
+      </a>
+    </div>
     {gitHubUserRepo['description'] && (
       <div className="text-gray-700 dark:text-gray-300">
         {gitHubUserRepo['description']}
       </div>
     )}
     {gitHubUserRepo['homepage'] && (
-      <a
-        className="hover:underline"
-        href={gitHubUserRepo['homepage']}
-        target={'_blank'}
-        aria-label={gitHubUserRepo['homepage']}
-        title={gitHubUserRepo['homepage']}
-      >
-        {gitHubUserRepo['homepage']}
-      </a>
+      <div>
+        <a
+          className="hover:underline"
+          href={gitHubUserRepo['homepage']}
+          target={'_blank'}
+          aria-label={gitHubUserRepo['homepage']}
+          title={gitHubUserRepo['homepage']}
+        >
+          {gitHubUserRepo['homepage']}
+        </a>
+      </div>
     )}
     <TopicsOrLanguages topicsOrLanguages={gitHubUserRepo['topics']} />
     <TopicsOrLanguages
